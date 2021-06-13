bd084f65d3724e4e02131b802cd447b04f806125
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-06-13 19:46:11 +0900

    コントローラーに処理を追加

--- app/controllers/api/notifications/unread_controller.rb
+++ app/controllers/api/notifications/unread_controller.rb
@@ -2,6 +2,18 @@
 
 class API::Notifications::UnreadController < API::BaseController
   def index
+    checked_unread_notifications = current_user.notifications.checked_unreads
+    checked_unread_notifications.each do |notification|
+      notification_path = notification[:path]
+      model = notification_path.split('/')[1].classify.constantize
+      id = notification_path.split('/')[2]
+      comments = model.find(id).comments
+      if comments.pluck(:user_id).include?(notification.sender.id)
+        latest_notification = current_user.notifications.unreads.where(path: notification_path)[0]
+        new_message = notification.message.gsub(/を確認しました。/, 'の確認とコメントをしました。')
+        notification.update(message: new_message, kind: 'check_and_comment', created_at: latest_notification.created_at + 3)
+      end
+    end
     @notifications = if params[:page]
                        current_user.notifications
                                    .unreads_with_avatar
