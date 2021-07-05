bdca58baa4bc2a154a81c19e2896740bb7a3de9f
kanekodaichi <knkdic.bfc@gmail.com>
2021-07-05 19:31:45 +0900

    テスト名を変更

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -250,7 +250,7 @@ class QuestionsTest < ApplicationSystemTestCase
     end
   end
 
-  test 'select practice title when question create page' do
+  test 'select practice title when push question botton on practice page' do
     visit_with_auth "/practices/#{practices(:practice23).id}", 'hatsuno'
     click_on '質問する'
     assert_text '[Ruby] rubyをインストールする'
